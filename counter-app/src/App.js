6ff9122af0ac9f5c287c5880fd51c9eeea5f4314
Niraj <codernj26@gmail.com>
2023-03-21 20:30:35 +0530

    Home work task completed functionality added for plus and minus

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -3,7 +3,29 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(1)
+  let [count, setCount] = useState(1)
+
+  const countPlus = ()=>{
+    // let temp = count
+    // temp += 1
+    // setCount(temp)
+
+    setCount(++count);
+    //  console.log(count) 
+
+    // apporach in which I'm facing doubt
+    setCount(count++);
+    // basically yaha pr pehle count set ho raha hai then increment ho raha hai since if i choose this type of updation i need to click button twice
+    // e.g. count = 1 i click on button + then it set count 1 then increment when i click second time the it set count 2 then increment, 
+  }
+
+  const countMinus = ()=>{
+    let temp = count
+    temp -= 1
+    setCount(temp)
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -11,7 +33,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload. {5*8}
         </p>
-        <h1>{count}</h1>
+        <div className='my-btn'>
+        <button className='btn info' onClick={countMinus} >-</button><h1>{count}</h1> <button className='btn info' onClick={countPlus}>+</button>
+        </div>
         {/* <button>Update Count</button> */}
         <a
           className="App-link"
